05a5e556ebede8ac235f34dfd29e3ba94f4e6746
mokshith-moksh <mokshithmokshith7@gmail.com>
2025-07-25 12:09:35 +0530

    addded-client-connection-working

--- app/controller/[roomId]/page.tsx
+++ app/controller/[roomId]/page.tsx
@@ -1,7 +1,59 @@
+"use client";
 import React from "react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useEffect } from "react";
+import { socket } from "@/socket";
 
 const Controller = () => {
-  return <div>Controller</div>;
+  const [connected, setConnected] = useState(false);
+  const params = useParams();
+  const roomId = params.roomId as string;
+  const [connectedClients, setConnectedClients] = useState(0);
+
+  useEffect(() => {
+    if (!roomId) return;
+
+    const onConnect = () => {
+      setConnected(true);
+      socket.emit("join-room", { roomId, role: "admin" });
+      console.log(`Joined room ${roomId} as admin`);
+    };
+
+    const onDisconnect = () => {
+      setConnected(false);
+      console.log(`Disconnected from room ${roomId}`);
+    };
+
+    socket.connect();
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("connected-client-count", ({ count }) => {
+      setConnectedClients(count);
+    });
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("connected-client-count");
+      socket.disconnect();
+    };
+  }, [roomId]);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-200 text-black">
+      <p className="text-black text-2xl mb-2">Controller View</p>
+      <p className="text-black text-lg">Room ID: {roomId}</p>
+      <p className="text-black mt-4">
+        Status: {connected ? "✅ Connected" : "❌ Disconnected"}
+      </p>
+      <p>Share this with Clients</p>
+      <p>http://localhost:3000/viewer/{roomId}</p>
+      <p className="text-green-500">Total Number of Clients connected</p>
+      <p className="mt-2 text-red-600">
+        👥 Live Connected Clients: {connectedClients ?? "loading..."}
+      </p>
+    </div>
+  );
 };
 
 export default Controller;
--- app/viewer/[roomId]/page.tsx
+++ app/viewer/[roomId]/page.tsx
@@ -1,7 +1,52 @@
+"use client";
 import React from "react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useEffect } from "react";
+import { socket } from "@/socket";
 
-const Viewer = () => {
-  return <div>Viewer</div>;
+const ViwerPage = () => {
+  const [connected, setConnected] = useState(false);
+  const params = useParams();
+
+  const roomId = params.roomId as string;
+
+  useEffect(() => {
+    if (!roomId) return;
+
+    const onConnect = () => {
+      setConnected(true);
+      socket.emit("join-room", { roomId, role: "client" });
+      console.log(`Joined room ${roomId} as client`);
+    };
+
+    const onDisconnect = () => {
+      setConnected(false);
+      console.log(`Disconnected from room ${roomId}`);
+    };
+
+    socket.connect();
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.disconnect();
+    };
+  }, [roomId]);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-slate-200">
+      <p className="text-black text-2xl mb-2">Viewer page</p>
+      <p className="text-black text-lg">Room ID: {roomId}</p>
+      <p className="text-black mt-4">
+        Status: {connected ? "✅ Connected" : "❌ Disconnected"}
+      </p>
+      <p>Share this with Clients</p>
+      <p>http://localhost:3000/viewer/{roomId}</p>
+    </div>
+  );
 };
 
-export default Viewer;
+export default ViwerPage;
